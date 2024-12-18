ef9cf982641861a3188820440ce42598788417a8
sameershahid1 <goku.hulk234@gmail.com>
2024-12-18 16:04:21 +0500

    feature: added the aks advance cluster package

--- charts/helm_repo.sh
+++ charts/helm_repo.sh
@@ -2,9 +2,36 @@
 
 ROOT_DIR="."
 OUTPUT_DIR="./packages"
+test_failed= false
 
 # Create the output directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
+
+echo "Testing each package...."
+
+find "$ROOT_DIR" -name "Chart.yaml" -print0 | while IFS= read -r -d '' chart; do
+    chart_dir=$(dirname "$chart")
+    echo "Processing chart directory: $chart_dir"
+
+    # Try block (Bash equivalent)
+    result=$(helm template "$chart_dir" 2>&1)  # Capture stderr and stdout
+    if [ $? -ne 0 ]; then
+        # If the command failed, print the error message and exit the script
+        echo "Error executing helm template for chart $chart_dir:"
+        echo "$result"  # Output the error message
+        test_failed=true
+        exit 1  # Exit the script with a non-zero status to indicate failure
+    else
+        # If the command was successful, print the result
+        echo "Helm package is tested for $chart_dir:"
+    fi
+done
+
+
+if ["$test_failed"=true]; then
+    echo "Exiting due to failure in helm template test."
+    exit 1
+fi
 
 echo "Checking for updated Helm charts..."
 
